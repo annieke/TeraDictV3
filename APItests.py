f18246c15ca3f7c97bc0891931fd8c191de7d10a
annie ke <annieke8@gmail.com>
2016-07-17 16:22:07 -0700

    Integrated module in test file

--- APItests.py
+++ APItests.py
@@ -1,12 +1,31 @@
 import requests 
 import json 
 
-import panlex_python_API.examples
+from panlex_python_API.panlex import query
 
 r = requests.post('http://api.panlex.org/lv', data = json.dumps({}))
 data = r.json()
 
 print(data["result"][0])
 
-print(translate("hello", eng-000, eng-000))
-# FIX THE IMPORT STATEMENTS!!!! 
+# Modified from examples.py in panlex_python_API module
+def translate(expn, startLang, endLang):
+    """Get the best-quality translation of expn, an expression in startLang, into endLang.
+    Languages are specified as PanLex UID codes (e.g. eng-000 for English.)"""
+    params1 = {"uid":startLang,
+               "tt":expn,
+               "indent":True}
+    r1 = query("/ex",params1)
+    exid = r1["result"][0]["ex"]
+    params2 = {"trex":exid,
+               "uid":endLang,
+               "indent":True,
+               "include":"trq", # include the field trq, "translation quality",
+               "sort":"trq desc", # sort by descending trq,
+               "limit":1} # get only one result (which will be the one with the highest score)}
+    r2 = query("/ex",params2)
+    return r2["result"][0]["tt"]
+
+
+translation = translate("hello", "eng-000", "eng-000")
+print(translation)
